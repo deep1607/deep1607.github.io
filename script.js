d70d226fd3209b406890395c202cf1829efe2364
Deep Shah <deepdsh@amazon.com>
2023-10-30 17:03:51 +0530

    Message 2

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const message = document.getElementById("message");
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
+const backButton = document.getElementById("backButton");
+
+function showInvalidResponse() {
+  message.textContent = "Invalid response. Please go back to the previous question.";
+  backButton.style.display = "block";
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+}
+
+function goBack() {
+  backButton.style.display = "none";
+  yesBtn.style.display = "block";
+  noBtn.style.display = "block";
+}
 
 yesBtn.addEventListener("click", function () {
   message.textContent = "Will you come out on a trip with me?";
@@ -15,21 +29,11 @@ yesBtn.addEventListener("click", function () {
       yesBtn.style.display = "none";
       noBtn.style.display = "none";
     });
-    noBtn.addEventListener("click", function () {
-      message.textContent = "Sorry to hear that. Maybe next time!";
-      yesBtn.style.display = "none";
-      noBtn.style.display = "none";
-    });
+    noBtn.addEventListener("click", showInvalidResponse);
   });
-  noBtn.addEventListener("click", function () {
-    message.textContent = "Oh, that's too bad. Maybe next time!";
-    yesBtn.style.display = "none";
-    noBtn.style.display = "none";
-  });
+  noBtn.addEventListener("click", showInvalidResponse);
 });
 
-noBtn.addEventListener("click", function () {
-  message.textContent = "Oh, that's too bad. Maybe next time!";
-  yesBtn.style.display = "none";
-  noBtn.style.display = "none";
-});
+noBtn.addEventListener("click", showInvalidResponse);
+
+backButton.addEventListener("click", goBack);
